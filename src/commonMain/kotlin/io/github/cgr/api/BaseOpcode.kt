2bea9b1445e8e2dfbba90de3cd116be2e3566ee9
Seggan <segganew@gmail.com>
2023-03-14 19:30:38 -0400

    Woops

--- src/commonMain/kotlin/io/github/cgr/api/BaseOpcode.kt
+++ src/commonMain/kotlin/io/github/cgr/api/BaseOpcode.kt
@@ -1,11 +1,8 @@
 package io.github.cgr.api
 
-import kotlin.reflect.KClass
-
 enum class BaseOpcode(
     override val argLength: Int,
-    private val executor: (IntArray, ArrayDeque<Any?>) -> Unit,
-    vararg args: KClass<*>
+    private val executor: (IntArray, ArrayDeque<Any?>) -> Unit
 ) : Opcode {
     NOP(0, { _, _ -> }),
     ;
